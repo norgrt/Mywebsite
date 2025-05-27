1f97e1b36535de7821ff243d6cea54f5234ad627
Ubuntu <ubuntu@ip-172-31-91-161.ec2.internal>
2025-05-27 18:12:44 +0000

    test 3

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
+
